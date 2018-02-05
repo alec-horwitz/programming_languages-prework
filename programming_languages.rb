73d821ad12189733c313c408a82f76616147fcbb
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-05 22:32:37 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each { |style, langs|
+    langs.each { |langName, attributes|
+      new_hash[langName] = attributes
+      binding.pry
+      new_hash[langName].include?(:style) ? new_hash[langName][:style].push(style) : new_hash[langName][:style] = [style]
+      binding.pry
+    }
+  }
+  new_hash
 end
